c455b2c113acce728d76357f7dbbae97f1fabf24
Daniel Cao <dyc33@cornell.edu>
2022-10-21 11:30:11 -0400

    Python Handler/Lib Creation

---
+++ python_template/handler.py
@@ -0,0 +1,10 @@
+import json
+
+
+def run(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully! There is an anomoly",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
