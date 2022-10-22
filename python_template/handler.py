a96a3a516cb3e371128f73bb3e0f0ab007e1e71b
Daniel Cao <dyc33@cornell.edu>
2022-10-22 19:18:38 -0400

    Add function url

--- python_template/handler.py
+++ python_template/handler.py
@@ -8,4 +8,10 @@ def run(event, context):
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
-    #vipins NEW test comment
+    # vipins NEW test comment
+
+# forms the URL for the EMCS API that returns the json representation of an object
+
+
+def emcsURL(point: str):
+    return "https: // www.emcs.cornell.edu /${point}?api =${API_KEY} & cmd = json"
