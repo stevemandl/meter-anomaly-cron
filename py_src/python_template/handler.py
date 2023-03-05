816572c41604d35828a8f77fcc0a12f9960c46cf
Jason Guo <sg763@cornell.edu>
2023-03-04 20:18:27 -0500

    correct dt problem

--- py_src/python_template/handler.py
+++ py_src/python_template/handler.py
@@ -1,9 +1,9 @@
 # python_template/handler.py
 from datetime import datetime, timedelta
 from requests.exceptions import ConnectionError, HTTPError
-
 from python_lib.utils import parse_event, fetch_trends
 import json
+import pandas as pd
 
 
 def run(event, context):
@@ -12,6 +12,7 @@ def run(event, context):
     now = datetime.now()
     # print(f"testTemplate run at {now} with event {event}")
     # parse event and ensure timeStamp and pointName are present
+    print(event)
     params = parse_event(event)
     end_time = params.get("timeStamp")
     start_time = end_time - timedelta(30)
@@ -21,14 +22,29 @@ def run(event, context):
         trend_response = fetch_trends(
             point=point_name, start_time=start_time, end_time=end_time
         )
+
         # at this point, status_code must be 200 or an exception would be raised
         # data should always be there, but just to be on the safe side, make an if statement
         if type(trend_response) is list:
             # this is the logic in the anomaly detection:
-            if len(trend_response[0]["datapoints"]) < 648:
+            # print(trend_response)
+
+            data = trend_response[0]['datapoints'] # the timestamps are multiplied by 1000 for some reason?
+            # print(data)
+            # dt = int(start_time.)
+
+            # assuming dt is an hour, this code works. but can't just do that D:
+            dt = int(timedelta(hours=1).total_seconds()*1000) 
+            num_hours_missing = 0
+            total = int((data[len(data)-1][1] - data[0][1])/dt)
+            for i in range(len(data) - 1):
+                print((data[i+1][1] - data[i][1])/dt)
+                if data[i+1][1] - data[i][1] != dt:
+                    num_hours_missing += int((data[i+1][1] - data[i][1])/dt)
+            if num_hours_missing > 0:
                 response[
                     "body"
-                ] = f"{point_name} is missing more than 10% of values for the period {start_time:%Y-%m-%d %H:%M} to {end_time:%Y-%m-%d %H:%M}"
+                ] = f"{point_name} is missing more than {int((num_hours_missing / total)*100)}% of values for the period {start_time:%Y-%m-%d %H:%M} to {end_time:%Y-%m-%d %H:%M}"
         else:  # response should always be a list
             response[
                 "body"
