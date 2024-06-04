ed591972251680f411cfc6232a534191d445b56b
Steve Mandl <stevemandl@users.noreply.github.com>
2024-06-04 11:25:54 -0400

    docs: Update handler_spec.py

--- py_src/python_template/handler_spec.py
+++ py_src/python_template/handler_spec.py
@@ -3,7 +3,44 @@ from requests.exceptions import HTTPError
 from requests.models import Response
 import json
 
+# See swagger docs at https://app.swaggerhub.com/apis/Cornell-BACSI/EMCS-portal/1.0.0
+
+# actual_testcase.json contains the response from the following JSON request body:
+# {
+#   "range": {
+#     "from": "2023-07-01T19:38:47.334Z",
+#     "to": "2023-07-30T19:38:47.334Z"
+#   },
+#   "interval": "30s",
+#   "intervalMs": 5500,
+#   "maxDataPoints": 50,
+#   "targets": [
+#     {
+#       "target": "DayHall.Elec.PowerScout3/kWsystem"
+#     }
+#   ]
+# }
 actual_testcase = open('python_template/Testcases/actual_testcase.json')
+
+# noanomaly_testcase.json contains the response from the following JSON request body:
+# {
+#   "panelId": "Q-1599986187842-0.164611811105138-0",
+#   "range": {
+#     "from": "2021-03-01T19:38:47.334Z",
+#     "to": "2021-03-30T19:38:47.334Z"
+#   },
+#   "interval": "30s",
+#   "intervalMs": 5500,
+#   "maxDataPoints": 50,
+#   "targets": [
+#     {
+#       "target": "StatlerHotel.Elec.PowerScout3/kWsystem"
+#     }
+#   ],
+#   "group": "string",
+#   "from": "2021-10-20T19:38:47.334Z",
+#   "to": "2021-10-21T19:38:47.334Z"
+# }
 noanomaly_testcase = open('python_template/Testcases/noanomaly_testcase.json')
 
 def test_nobarf(mocker):
@@ -66,21 +103,6 @@ def test_actual(mocker):
     result = run(event, None)
     assert "statusCode" in result
     assert "missing more than 10%" in result.get("body")
-#   Swagger JSON request body https://app.swaggerhub.com/apis/Cornell-BACSI/EMCS-portal/1.0.0
-#. {
-#   "range": {
-#     "from": "2023-07-01T19:38:47.334Z",
-#     "to": "2023-07-30T19:38:47.334Z"
-#   },
-#   "interval": "30s",
-#   "intervalMs": 5500,
-#   "maxDataPoints": 50,
-#   "targets": [
-#     {
-#       "target": "DayHall.Elec.PowerScout3/kWsystem"
-#     }
-#   ]
-# }
 
 def test_noanomly(mocker):
     event = {
@@ -97,22 +119,3 @@ def test_noanomly(mocker):
     result = run(event, None)
     assert "statusCode" in result
     assert "" == result.get("body")
-#     Swagger JSON request body https://app.swaggerhub.com/apis/Cornell-BACSI/EMCS-portal/1.0.0
-# {
-#   "panelId": "Q-1599986187842-0.164611811105138-0",
-#   "range": {
-#     "from": "2021-03-01T19:38:47.334Z",
-#     "to": "2021-03-30T19:38:47.334Z"
-#   },
-#   "interval": "30s",
-#   "intervalMs": 5500,
-#   "maxDataPoints": 50,
-#   "targets": [
-#     {
-#       "target": "StatlerHotel.Elec.PowerScout3/kWsystem"
-#     }
-#   ],
-#   "group": "string",
-#   "from": "2021-10-20T19:38:47.334Z",
-#   "to": "2021-10-21T19:38:47.334Z"
-# }
