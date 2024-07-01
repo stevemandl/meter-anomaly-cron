1c46c715c8f90a86e1fa3bdafe457f581a971fd6
logo0303 <you@example.com>
2024-07-01 20:06:09 +0000

    Feat: test cases and tests added

--- py_src/long_term_reading_variance/longterm_spec.py
+++ py_src/long_term_reading_variance/longterm_spec.py
@@ -41,7 +41,74 @@ noanomaly_testcase = open('python_template/Testcases/noanomaly_testcase.json')
 #   "from": "2021-10-20T19:38:47.334Z",
 #   "to": "2021-10-21T19:38:47.334Z"
 # }
+five_months_old = open('long_term_reading_variance/Testcases/five_months_old.json')
+"""
+five_months_old.json is a file containing the swagger JSON response from the following query 
+{
+  "range": {
+    "from": "2019-04-01T19:38:47.334Z",
+    "to": "2019-04-30T19:38:47.334Z"
+  },
+  "interval": "30s",
+  "intervalMs": 5500,
+  "maxDataPoints": 50,
+  "targets": [
+    {
+      "target": "BakerLab.Elec.South.PowerScout3/kW_System"
+    }
+  ]
+}
+"""
+five_months_new = open('long_term_reading_variance/Testcases/five_months_new.json')
+"""
+five_months_new.json is a file containing the swagger JSON response from the following query 
+{
+  "range": {
+    "from": "2020-04-01T19:38:47.334Z",
+    "to": "2020-04-30T19:38:47.334Z"
+  },
+  "interval": "30s",
+  "intervalMs": 5500,
+  "maxDataPoints": 50,
+  "targets": [
+    {
+      "target": "BakerLab.Elec.South.PowerScout3/kW_System"
+    }
+  ]
+}
+"""
 
+one_month_old = open('long_term_reading_variance/Testcases/one_month_old.json')
+"""
+one_month_old.json is a file containing the swagger JSON response from the following query 
+{
+  "range": {
+    "from": "2020-8-19",
+    "to": "2020-9-18"
+  },
+  "targets": [
+    {
+      "target": "CarpenterHall.CW.FP/TONS"
+    }
+  ]
+}
+"""
+
+one_month_new = open('long_term_reading_variance/Testcases/one_month_new.json')
+"""
+one_month_old.json is a file containing the swagger JSON response from the following query 
+{
+  "range": {
+    "from": "2021-8-19",
+    "to": "2021-9-18"
+  },
+  "targets": [
+    {
+      "target": "CarpenterHall.CW.FP/TONS"
+    }
+  ]
+}
+"""
 def test_nobarf(mocker):
     event = {
         "body": {
@@ -127,11 +194,54 @@ def test_onetime(mocker):
             "timeStamp": "2022-10-05T23:58:47.390Z",
         }
     }
-    data = json.load(noanomaly_testcase)
+    #data1 contains the data from the previous year 
+    #data2 contains the data from the target year
+    data1 = json.load(five_months_old)
+    data2 = [{"datapoints": []}]
     mocker.patch(
         "python_template.handler.fetch_trends",
-        return_value = data,
+        return_value = [data1, data2]
     )
     result = run(event, None)
     assert "statusCode" in result
-    assert "" == result.get("body")
+    assert "missing timeframe" == result.get("body")
+    
+def test_five_months(mocker):
+    event = {
+        "body": {
+            "pointName": "KlarmanHall.Elec.Solar.PowerScout3037/kW_System",
+            "timeStamp": "2022-10-05T23:58:47.390Z",
+        }
+    }
+    #data1 contains the data from the previous year 
+    #data2 contains the data from the target year
+    data1 = json.load(five_months_old)
+    data2 = json.load(five_months_new)
+    
+    mocker.patch(
+        "python_template.handler.fetch_trends",
+        return_value = [data1, data2]
+    )
+    result = run(event, None)
+    assert "statusCode" in result
+    assert "anomaly detected" == result.get("body")
+    
+def test_one_month(mocker):
+    event = {
+        "body": {
+            "pointName": "KlarmanHall.Elec.Solar.PowerScout3037/kW_System",
+            "timeStamp": "2022-10-05T23:58:47.390Z",
+        }
+    }
+    #data1 contains the data from the previous year 
+    #data2 contains the data from the target year
+    data1 = json.load(one_month_old)
+    data2 = json.load(one_month_new)
+    
+    mocker.patch(
+        "python_template.handler.fetch_trends",
+        return_value = [data1, data2]
+    )
+    result = run(event, None)
+    assert "statusCode" in result
+    assert "anomaly detected" == result.get("body")
