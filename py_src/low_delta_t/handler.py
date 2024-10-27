3e4bad37194dc7906f3b188ede88fa53ced1c7ba
stevemandl <sjm34@cornell.edu>
2024-10-27 09:37:59 -0400

    set weighted_model_dt before access

--- py_src/low_delta_t/handler.py
+++ py_src/low_delta_t/handler.py
@@ -63,6 +63,7 @@ def run(event, _context):
         # compare actual delta-t with modeled normal, and detect anomaly if it falls below model by more than variance
         actual_dt = np.mean(model_df[rtemp_name] - model_df[stemp_name])
         weighted_actual_dt = None
+        weighted_model_dt = None
         model_dt = np.mean(model_df["DT_PRED"])
         if np.sum(model_df[flow_name]) > 0:
             weighted_actual_dt = np.average(model_df[rtemp_name] - model_df[stemp_name], weights=model_df[flow_name])
